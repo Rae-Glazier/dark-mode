b6984094f939f5d7bfc51b30261bd94b96f7989d
rae-glazier <glazier.home.life@gmail.com>
2019-12-04 21:17:56 -0600

    i need to debug

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,21 +1,23 @@
 import { useEffect} from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
-export const useDarkMode = () => {
+const useDarkMode = () => {
 
-    const [darkMode, setDarkMode] = useLocalStorage('dark-mode');
+    const [enableDark, setEnableDark] = useLocalStorage('dark-mode-enabled');
 
     useEffect( () => {
-        
+
         const className = 'dark-mode';
         const element = window.document.body;
 
-        if (darkMode) {
+        if (enableDark) {
             element.classList.add(className);
         } else {
             element.classList.add(className);
         }
-    }, [darkMode] );
+    }, [enableDark] );
 
-    return [darkMode, setDarkMode];
+    return [enableDark, setEnableDark];
 };
+
+export default useDarkMode;
